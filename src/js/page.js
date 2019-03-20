fe28a51a9e1f439bf8505ac436784f83e319ec11
E179823K <alban.moizan@etu.univ-nantes.fr>
2019-03-20 11:27:25 +0100

    JS

--- src/js/page.js
+++ src/js/page.js
@@ -3,20 +3,41 @@
 $(document).ready(function () {
 
     init();
-    let input = $('#input_ville');
-    $('#input_ville').on( 'input', function () {
 
-        input.vicopo(function (value) {
-            //$('#vicopo').text(JSON.stringify(value, null, 2));
-            $('#list_proposition').empty();
-            value.forEach((ville) => {
-                $('#list_proposition').append('<option value="'+ville.city+', '+ville.code+'">');
-            })
-        });
+    let input = $('#input_ville');
+    input.on('input', function () {
+        if(isNaN(input.val()) && (input.val().length >=3)) {
+            $.ajax({
+                method: "GET",
+                url: "https://geo.api.gouv.fr/communes?",
+                data: {
+                    nom: input.val(),
+                    fields : "nom",
+                },
+                context: document.body
+            }).done(function (json) {
+                let prop = $('#list_proposition');
+                prop.empty();
+                json.forEach((ville) => {
+                    prop.append('<option value="'+ville.nom+'">');
+                });
+
+            });
+        }
     });
 
 
 
+   /* $('#input_ville').on( 'input', function () {
+
+        input.vicopo(function (value) {
+            //$('#vicopo').text(JSON.stringify(value, null, 2));
+
+            value.forEach((ville) => {
+                $('#list_proposition').append('<option value="'+ville.city+', '+ville.code+'">');
+            })
+        });
+    });*/
 
 
 });
